0144040363bbc17095c219e5788918cdbf07d472
Willian <willian.vieira@usherbrooke.ca>
2021-11-28 19:09:34 -0500

    more doc

--- echantillonnage.R
+++ echantillonnage.R
@@ -68,47 +68,14 @@ sonometres <- sonometres[!sonometres %in% sonometres_a_enlever]
 
 
 
-###############################################################################
-# Première boucle par sonomètre pour vérifier si la classification et les échantillons sont corrects:
-# - lister toutes les fichiers pour 1 sonomètre
-# - Classifier les fichiers dans le periode de nidification + periode de la journée (Aurore, Crépuscule, et Nuit)
-# - Creer deux graphique pdf à l'interieur du dossier de chacun des sonomètres
-###############################################################################
-
-for(sono in sonometres)
-{
-    # imprimer dans la console le nom du sonometre pour vérifier le progress de la boucle
-    cat('\n\n', paste0(rep('#', 50), collapse = ''), '\n  Sonomètre:', sono, '\n', paste0(rep('#', 50), collapse = ''))
-
-    # lister toutes les fichiers pour le sonometre (filtrer les fichiers avec plus de 34500 Kb)
-    sono_dt <- listAudio_sm4(Dir = sono, filterSize = 34500)
-
-    # Classifier la liste des fichiers dans les periodes de nidification et dans la journée (A1 - E6)
-    sonoClass_dt <- classify_period(sono_dt,
-                                    startDate = lubridate::as_date('2020-05-30'),
-                                    endDate = lubridate::as_date('2020-07-23'),
-                                    groups = 5)
-
-    # Échantillonner les fichiers
-    sono_echantillonne <- sample_files(sonoClass_dt, sampleSize = 1, overSample = 1)
-
-    # Générer une figure avec tous les fichiers distribué par periode de nidification et dans la journée
-    # `colorDay` définit si on va différencier (TRUE) ou non (FALSE) les périodes de la journée avec une couleur
-    plot_files(sonoClass_dt, colorDay = TRUE)
-
-    # Générer une deuxième figure avec tous les fichiers + les fichiers echantillonés colorés
-    # `fileSuffix` consiste à ajouter un suffixe dans le nom du fichier pour éviter l'écrasement
-    plot_files(sonoClass_dt, sono_echantillonne, colorDay = FALSE, fileSuffix = '_echantillon')
-}
-
-
-
 
 ###############################################################################
-# Dexième boucle par sonomètre pour la selection des fichiers:
-# - lister toutes les fichiers pour 1 sonomètre
-# - Classifier les fichiers dans le periode de nidification + periode de la journée (Aurore, Crépuscule, et Nuit)
-# - Creer deux graphique pdf à l'interieur du dossier de chacun des sonomètres
+# Boucle par sonomètre pour la selection des fichiers avec les etapes:
+# 1. lister toutes les fichiers pour le sonomètre i
+# 2. Classifier les fichiers dans le periode de nidification + periode de la journée (Aurore, Crépuscule, et Nuit)
+# 3. Creer deux graphique pdf à l'interieur du dossier de chacun des sonomètres
+# 4. 
+# 5. 
 ###############################################################################
 
 for(sono in sonometres)
@@ -116,26 +83,57 @@ for(sono in sonometres)
     # imprimer dans la console le nom du sonometre pour vérifier le progress de la boucle
     cat('\n\n', paste0(rep('#', 50), collapse = ''), '\n  Sonomètre:', sono, '\n', paste0(rep('#', 50), collapse = ''))
 
-    # lister toutes les fichiers pour le sonometre (filtrer les fichiers avec plus de 34500 Kb)
-    sono_dt <- listAudio_sm4(Dir = sono, filterSize = 34500)
+
+    # lister toutes les fichiers pour le sonometre
+    # `sizeRange_*` defini les limites min et max de taille (en KB) pour les fichiers 3 minutes et 10 minutes
+    sono_dt <- listAudio(
+        Dir = sono,
+        sizeRange_3 = c(30000, 40000),
+        sizeRange_10 = c(100000, 110000)
+    )
+
 
     # Classifier la liste des fichiers dans les periodes de nidification et dans la journée (A1 - E6)
-    sonoClass_dt <- classify_period(sono_dt,
-                                    startDate = lubridate::as_date('2020-05-30'),
-                                    endDate = lubridate::as_date('2020-07-23'),
-                                    groups = 5)
+    # `groups` define en combien la period de temps (startDate - endDate) sera partagé
+    sonoClass_dt <- classify_period(
+        dt = ono_dt,
+        startDate = lubridate::as_date('2020-05-30'),
+        endDate = lubridate::as_date('2020-07-23'),
+        groups = 5
+    )
+
 
     # Échantillonner les fichiers
-    sono_echantillonne <- sample_files(sonoClass_dt, sampleSize = 1, overSample = 1)
+    # `sampleSize` et `overSample` pour chaque period de la journée ET groupe
+    sono_echantillonne <- sample_files(
+        dt = sonoClass_dt,
+        sampleSize = 1,
+        overSample = 1
+    )
+
 
     # Générer une figure avec tous les fichiers distribué par periode de nidification et dans la journée
     # `colorDay` définit si on va différencier (TRUE) ou non (FALSE) les périodes de la journée avec une couleur
-    plot_files(sonoClass_dt, colorDay = TRUE)
+    # la figure sera sauvegardé dedans le dossier du sonomètre
+    plot_files(
+        dt = sonoClass_dt,
+        colorDay = TRUE
+    )
+
 
     # Générer une deuxième figure avec tous les fichiers + les fichiers echantillonés colorés
-    # `fileSuffix` consiste à ajouter un suffixe dans le nom du fichier pour éviter l'écrasement
-    plot_files(sonoClass_dt, sono_echantillonne, colorDay = FALSE, fileSuffix = '_echantillon')
+    # `fileSuffix` consiste à ajouter un suffixe dans le nom du fichier pour éviter l'écrasement de celui géneré dans l'étape precedent
+    plot_files(
+        dt = sonoClass_dt,
+        sampled = sono_echantillonne,
+        colorDay = FALSE,
+        fileSuffix = '_echantillon'
+    )
+
 
     # Déplacer les fichiers
-    move_files(sono_echantillonne)
+    move_files(
+        sampled = sono_echantillonne
+    )
+
 }
